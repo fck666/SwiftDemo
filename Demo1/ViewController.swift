14152feec0bfaf1c0b9782816ca5b748c2c36c02
冯晨 <fengchen04@58.com>
2021-06-01 20:11:04 +0800

    圆角

--- Demo1/ViewController.swift
+++ Demo1/ViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 //@available(iOS 14.0, *)
 class ViewController: UITabBarController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tabBar.barTintColor = UIColor.white
@@ -18,6 +20,7 @@ class ViewController: UITabBarController {
         addChildVC(childController: FirstViewController(), childTitle: "First", imageName: "01", selectedImageName: "1fill")
         addChildVC(childController: SecViewController(), childTitle: "Second", imageName: "02", selectedImageName: "2fill")
         addChildVC(childController: ThirdViewController(), childTitle: "Third", imageName: "03", selectedImageName: "3fill")
+//        self.viewControllers = [FirstViewController(),SecViewController(),ThirdViewController()]
         // Do any additional setup after loading the view.
     }
     private func addChildVC(childController: UIViewController, childTitle: String, imageName: String,selectedImageName: String) {
@@ -31,7 +34,6 @@ class ViewController: UITabBarController {
         
         childController.tabBarItem.image = UIImage(named: imageName)
         childController.tabBarItem.selectedImage = UIImage(named: selectedImageName)
-        
         self.addChild(navigation)
     }
 

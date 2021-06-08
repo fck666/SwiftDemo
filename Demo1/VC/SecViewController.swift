9a62ac3f7c03983edd5cb570e45529b00554452b
wangkangkang01 <wangkangkang01@58.com>
2021-06-08 17:10:59 +0800

    delegate刷新

--- Demo1/VC/SecViewController.swift
+++ Demo1/VC/SecViewController.swift
@@ -14,6 +14,7 @@ import Kingfisher
 import MJRefresh
 class SecViewController: UIViewController{
     let header = MJRefreshNormalHeader()
+    var tabRefreshDelegate : TabbarRefreshDelegate?
     @objc func tapped(mes : String){
         let des = MesViewController()
         des.message = mes
@@ -119,4 +120,10 @@ extension SecViewController :UITableViewDelegate,UITableViewDataSource{
     }
     
     
+}
+
+extension SecViewController: TabbarRefreshDelegate {
+    func refresh() {
+        table.mj_header?.beginRefreshing()
+    }
 }
